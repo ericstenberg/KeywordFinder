39d3eb839b13ec3eee174ee722dd20cff565ea1b
Eric Stenberg <eric.d.stenberg@gmail.com>
2020-08-24 00:01:45 -0400

    Finishec cleandata.py.

--- cleandata.py
+++ cleandata.py
@@ -2,40 +2,29 @@
 # License: GNU Affero General Public License v3.0
 
 import string as s
-import pandas as pd
-import unicodedata
+
 
 class CleanData:
     def __init__(self):
-        self._cleanSeries = pd.Series([], dtype='string')
+        self._cleanSeries = []
 
     def setCleanSeries(self, rawKeywordsList):
-    # remove hyperlinks
-        #exclude = s.punctuation
 
         for idx in range(0, len(rawKeywordsList)):
-            rawKeywordsList[idx] = rawKeywordsList[idx].replace(unicode.translate(s.punctuation), ' ')
-            rawKeywordsList[idx] = rawKeywordsList[idx].replace(unicode.translate(s.whitespace), ' ')
+            rawKeywordsList[idx] = rawKeywordsList[idx].replace('\n', ' ')
+            rawKeywordsList[idx] = rawKeywordsList[idx].translate(
+                rawKeywordsList[idx].maketrans('', '', s.punctuation))
+            rawKeywordsList[idx] = rawKeywordsList[idx].replace('''©''', '')
             rawKeywordsList[idx] = rawKeywordsList[idx].lower()
-            print(rawKeywordsList[idx])
+            rawKeywordsList[idx] = ' '.join(rawKeywordsList[idx].split())
 
-
-#        ['' if ]
- #       for item in rawKeywordsSeries:
-  #          self._cleanSeries = self._cleanSeries.append(pd.Series(
-   #             item.lower(), dtype='string'))
+        self._cleanSeries = rawKeywordsList
 
     def getCleanSeries(self):
         return self._cleanSeries
 
-    def setKeywordCountSeries(self):
-        pass
-
-    def getKeywordCountSeries(self):
-        pass
 
 def cleanData(rawKeywordsList):
     var = CleanData()
     var.setCleanSeries(rawKeywordsList)
-#    print(var.getCleanSeries())
-#    return var.getCleanSeries()
+    return var.getCleanSeries()
